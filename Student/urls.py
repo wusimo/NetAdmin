7aa12ca9bf9f7f95eb028498a04aec88572dee1b
SteveLeeLX <steveleelx@gmail.com>
2015-07-29 00:26:18 +0800

    student url struct

---
+++ Student/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from Student import views
+
+urlpatterns = [
+    url(r'^email/', views.email_page_view),
+    url(r'^toadmin/', views.toadmin_page_view),
+    url(r'^tota/', views.tota_page_view),
+    url(r'^toparents/', views.toparents_page_view),
+    url(r'^toprof/', views.toprof_page_view),
+]
