8d49d942c687c30ef8444e877455c360faefcdfa
SteveLeeLX <steveleelx@gmail.com>
2015-07-28 15:21:25 +0800

    fix mailbox structure

--- NetAdmin/urls.py
+++ NetAdmin/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from NetAdmin import views as NetAdmin_view
+
 urlpatterns = [
+    url(r'^$', NetAdmin_view.landing_page_view, name='landing'),
     url(r'^admin/', include(admin.site.urls)),
 ]
