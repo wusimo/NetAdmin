97b7cd04a74d2e45cc1bd071c4d22c20b1bdcd5e
BakaBBQ <laoboqi@126.com>
2015-07-29 11:56:54 +0800

    added admin urls

--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def tostudent_page_view(request, student_id):
+    return HttpResponse('tostudent ' + str(student_id))
+    
+def tostudent_toschool_page_view(request, student_id):
+    return HttpResponse('tostudent_toschool ' + str(student_id))
+
+def tota_page_view(request, ta_id):
+    return HttpResponse('tota ' + str(ta_id))
+    
+def toparents_page_view(request, parents_id):
+    return HttpResponse('toparents ' + str(parents_id))
+    
+def dashboard_page_view(request):
+    return HttpResponse('dashboard')
