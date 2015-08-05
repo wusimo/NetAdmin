67e63e37e8c08a59f2bf323661afbed338a379f5
SteveLeeLX <steveleelx@gmail.com>
2015-08-05 20:47:21 +0800

    message model

--- Student/models.py
+++ Student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Account.models import Account
 
 # Create your models here.
+
+class Message(models.Model):
+    sender = models.CharField(max_length=200)
+    receiver = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    send_time = models.DateTimeField()
+    unread = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.title
