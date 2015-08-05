80b7ab8e2da4e7b5206f6051d2cdb3b5a5e6dc27
SteveLeeLX <steveleelx@gmail.com>
2015-08-05 20:53:03 +0800

    landing page

--- NetAdmin/views.py
+++ NetAdmin/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index_page_view(request):
     return render(request, 'mailbox.html')
+
+def landing_page_view(request):
+    return HttpResponse('Here is landing page')
